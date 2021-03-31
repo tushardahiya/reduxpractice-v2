960a9985cf38481a156ca681e7623e2038a0316c
tushardahiya <tushardahiya84@gmail.com>
2021-04-01 00:34:11 +0530

    utilized cleanup function in useeffect for clearing the previous timout function timer on every keystroke when its run again

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,47 +1,52 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from "react";
 
-import IngredientForm from './IngredientForm';
-import IngredientList from './IngredientList';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
 const Ingredients = () => {
-  const [userIngredients , setUserIngredients ] = useState([]);
+  const [userIngredients, setUserIngredients] = useState([]);
 
-  useEffect( () => {
-    console.log('RENDERING INGREDIENTS', userIngredients);
-  }, [userIngredients])
+  useEffect(() => {
+    console.log("RENDERING INGREDIENTS", userIngredients);
+  }, [userIngredients]);
 
-  //usecallback to the filteredingredientshandler so that it 
-  //doesnt render again and again on executing because it 
+  //usecallback to the filteredingredientshandler so that it
+  //doesnt render again and again on executing because it
   //also causes  rerender
-  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setUserIngredients(filteredIngredients);
-  } , [])
+  }, []);
 
-  const onAddIngredientHandler = ingredient => {
-    fetch('https://hookspracticev2-default-rtdb.firebaseio.com/ingredients.json' , {
-      method:'POST',
-      body:JSON.stringify(ingredient),
-      headers: { 'Content-Type':'application/json' }
-    }).then(response => {
-      return response.json();
-    }).then( responseData => {
-        setUserIngredients( prevIngredients => [
-          ...prevIngredients , 
-          { id:responseData.name, ...ingredient}
+  const onAddIngredientHandler = (ingredient) => {
+    fetch(
+      "https://hookspracticev2-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
         ]);
-    });
+      });
   };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient= {onAddIngredientHandler} />
+      <IngredientForm onAddIngredient={onAddIngredientHandler} />
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
-        <IngredientList ingredients={userIngredients} onRemoveItem={ () => {}}/>
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
