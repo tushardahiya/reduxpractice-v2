fd313ae397541ead2712129c725808139f4e212b
tushardahiya <tushardahiya84@gmail.com>
2021-03-31 16:52:13 +0530

    sent ingredientdata to firebase using fetch(not axios)

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,14 +8,23 @@ const Ingredients = () => {
   const [userIngredients , setUserIngredients ] = useState([]);
 
   const onAddIngredientHandler = ingredient => {
-    setUserIngredients( prevIngredients => 
-      [...prevIngredients , { id:Math.random.toString , ...ingredient}])
+    fetch('https://hookspracticev2-default-rtdb.firebaseio.com/ingredients.json' , {
+      method:'POST',
+      body:JSON.stringify(ingredient),
+      headers: { 'Content-Type':'application/json' }
+    }).then(response => {
+      return response.json();
+    }).then( responseData => {
+        setUserIngredients( prevIngredients => [
+          ...prevIngredients , 
+          { id:responseData.name, ...ingredient}
+        ]);
+    });
   };
 
   return (
     <div className="App">
       <IngredientForm onAddIngredient= {onAddIngredientHandler} />
-
       <section>
         <Search />
         <IngredientList ingredients={userIngredients} onRemoveItem={ () => {}}/>
